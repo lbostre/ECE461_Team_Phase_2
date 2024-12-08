93526ad714e8db7f9fa1b23d5dc1f700207daefa
Anish Sudini <anish.sudini556@gmail.com>
2024-12-08 15:14:41 -0500

    Update to yaml file to latest open-api spec, optimized fetching issues and commits for larger repos

--- src/util/fetchData.ts
+++ src/util/fetchData.ts
@@ -36,36 +36,33 @@ export async function fetchCommits(
     commitsUrl: string,
     headers: { Accept: string; Authorization: string }
 ) {
-    // Correct and validate the URL
     const correctedUrl = correctGitHubUrl(commitsUrl, 'commits');
-
     const pageSize = 100;
     const maxParallelRequests = 5;
-    let page = 1;
     const uniqueContributors = new Map<string, number>();
+
+    let page = 1;
     let keepFetching = true;
 
     while (keepFetching) {
-        const requests = [];
-        for (let i = 0; i < maxParallelRequests; i++) {
-            requests.push(
+        const responses = await Promise.all(
+            Array.from({ length: maxParallelRequests }, (_, i) =>
                 axios.get(`${correctedUrl}?page=${page + i}&per_page=${pageSize}`, { headers })
-            );
-        }
+            )
+        );
 
-        const responses = await Promise.all(requests);
-        responses.forEach((response) => {
-            if (response.data.length === 0) {
-                keepFetching = false;
-                return;
+        keepFetching = false;
+        for (const response of responses) {
+            if (response.data.length > 0) {
+                keepFetching = true;
+                response.data.forEach((commit: { author: { login: string } }) => {
+                    const login = commit.author?.login;
+                    if (login) {
+                        uniqueContributors.set(login, (uniqueContributors.get(login) || 0) + 1);
+                    }
+                });
             }
-            response.data.forEach((commit: { author: { login: any } }) => {
-                const login = commit.author?.login;
-                if (login) {
-                    uniqueContributors.set(login, (uniqueContributors.get(login) || 0) + 1);
-                }
-            });
-        });
+        }
 
         page += maxParallelRequests;
     }
@@ -77,48 +74,46 @@ export async function fetchIssues(
     issuesUrl: string,
     headers: { Accept: string; Authorization: string }
 ) {
-    // Correct and validate the URL
     const correctedUrl = correctGitHubUrl(issuesUrl, 'issues');
-
     const pageSize = 100;
     const maxParallelRequests = 5;
+
     let page = 1;
-    let openIssues = 0,
-        closedIssues = 0;
+    let openIssues = 0;
+    let closedIssues = 0;
     const issueDurations: number[] = [];
+
     let keepFetching = true;
 
     while (keepFetching) {
-        const requests = [];
-        for (let i = 0; i < maxParallelRequests; i++) {
-            requests.push(
+        const responses = await Promise.all(
+            Array.from({ length: maxParallelRequests }, (_, i) =>
                 axios.get(`${correctedUrl}?page=${page + i}&per_page=${pageSize}&state=all`, {
                     headers,
                 })
-            );
-        }
+            )
+        );
 
-        const responses = await Promise.all(requests);
-        responses.forEach((response) => {
-            if (response.data.length === 0) {
-                keepFetching = false;
-                return;
-            }
-            response.data.forEach(
-                (issue: { closed_at: string | number | Date; created_at: string | number | Date }) => {
-                    if (issue.closed_at) {
-                        const duration =
-                            (new Date(issue.closed_at).getTime() -
-                                new Date(issue.created_at).getTime()) /
-                            (1000 * 3600 * 24);
-                        issueDurations.push(duration);
-                        closedIssues++;
-                    } else {
-                        openIssues++;
+        keepFetching = false;
+        for (const response of responses) {
+            if (response.data.length > 0) {
+                keepFetching = true;
+                response.data.forEach(
+                    (issue: { closed_at: string | number | Date; created_at: string | number | Date }) => {
+                        if (issue.closed_at) {
+                            const duration =
+                                (new Date(issue.closed_at).getTime() -
+                                    new Date(issue.created_at).getTime()) /
+                                (1000 * 3600 * 24);
+                            issueDurations.push(duration);
+                            closedIssues++;
+                        } else {
+                            openIssues++;
+                        }
                     }
-                }
-            );
-        });
+                );
+            }
+        }
 
         page += maxParallelRequests;
     }
